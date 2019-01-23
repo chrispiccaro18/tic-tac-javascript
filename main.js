10433589478b0a294622eadf77f4c0ec9c36581e
Chris Piccaro <chrispiccaro18@gmail.com>
2019-01-22 16:26:25 -0800

    Trying to figure out events

--- main.js
+++ main.js
@@ -1,49 +1,34 @@
+var player = 1;
+
+var topLeft, topMiddle, topRight, middleLeft, middleMiddle, middleRight, bottomLeft, bottomMiddle, bottomRight;
+
+var board = [
+    topLeft = document.getElementById('topLeft'),
+    topMiddle = document.getElementById('topMiddle'),
+    topRight = document.getElementById('topRight'),
+    middleLeft = document.getElementById('middleLeft'),
+    middleMiddle = document.getElementById('middleMiddle'),
+    middleRight = document.getElementById('middleRight'),
+    bottomLeft = document.getElementById('bottomLeft'),
+    bottomMiddle = document.getElementById('bottomMiddle'),
+    bottomRight = document.getElementById('bottomRight'),
+];
+
 function play() {
-
-    var topLeft;
-    var topMiddle;
-    var topRight;
-    var middleLeft;
-    var middleMiddle;
-    var middleRight;
-    var bottomLeft;
-    var bottomMiddle;
-    var bottomRight;
-
-    var board = [
-        topLeft = document.getElementById('topLeft'),
-        topMiddle = document.getElementById('topMiddle'),
-        topRight = document.getElementById('topRight'),
-        middleLeft = document.getElementById('middleLeft'),
-        middleMiddle = document.getElementById('middleMiddle'),
-        middleRight = document.getElementById('middleRight'),
-        bottomLeft = document.getElementById('bottomLeft'),
-        bottomMiddle = document.getElementById('bottomMiddle'),
-        bottomRight = document.getElementById('bottomRight'),
-    ];
-
-    console.log(board[0]);
-
-    // topLeft.innerHTML = 'X';
-
-    var player = 1;
 
     var playerName = document.getElementById('playerNameDisplay');
     playerName.innerHTML = `It is Player ${player}'s turn.`;
 
-    var board = document.getElementsByClassName('boardButton');
+    // var board = document.getElementsByClassName('boardButton');
 
-    function executePlayerMove() {
+}
 
+function executePlayerMove(board) {
+    var elMove = document.getElementsByClassName('board');
+    elMove.addEventListener('click', function(e) {
+        document.table.appendChild('X');
+    });
 
-        if(player === 1) {
-            var currentMove = document.getElementById();
-            currentMove.innerHTML = 'X';
-        }
-    }
-    //     switch (board[]) {
-    //         case 0:
-    //         // code
-    //         break;
-    //     }
+    console.log(elMove);
+
 }
